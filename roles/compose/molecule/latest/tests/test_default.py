5977abf2b9fc266659d6a078c1bec952933ee62e
Bodo Schulz <bodo@boone-schulz.de>
2026-03-17 21:36:39 +0100

    update all roles

---
+++ roles/compose/molecule/latest/tests/test_default.py
@@ -0,0 +1,29 @@
+
+from __future__ import annotations, unicode_literals
+
+import os
+
+import pytest
+import testinfra.utils.ansible_runner
+from helper.molecule import get_vars, infra_hosts, local_facts
+
+testinfra_hosts = infra_hosts(host_name="instance")
+
+# --- tests -----------------------------------------------------------------
+
+
+@pytest.mark.parametrize("dirs", [
+    "/usr/local/opt/docker-compose",
+])
+def test_directories(host, dirs):
+
+    d = host.file(dirs)
+    assert d.is_directory
+
+@pytest.mark.parametrize("files", [
+    "/usr/bin/docker-compose",
+])
+def test_files(host, dirs):
+
+    d = host.file(dirs)
+    assert d.is_file
